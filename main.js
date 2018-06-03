b5d244fd7fa643e276ab7284abe35f380ce37efb
YuvalGoldschmidt <yuviyuvi258@gmail.com>
2018-06-03 16:53:52 +0300

    Using AngularJS

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
 var main=function(){
-	$('.filter-choose-all').click(function(){
-		if($(this).not('.active')){
-			$('.filter-choose').removeClass('active');
-			$('.filter-choose-all').addClass('active');
-			$('.profile').show();
-		}
-	});
-
-	$('.filter-choose').click(function(){
-		$(this).toggleClass('active');
-		var $filteractiveid = $(this).attr('id');
-		$('div.profile'+'.'+$filteractiveid).toggle();
-		
-		if ($('.filter-choose-all').hasClass('active')) {
-			$('.filter-choose-all').removeClass('active');			
-			$('.profile').hide();
-			$('div.profile'+'.'+$filteractiveid).show();
-		}
-		else if (($('.filter:has(div.active)').length)==($('.filter:has(div.filter-choose)').length) || ($('.filter:has(div.active)').length)==0) {
-			$('div.filter-choose').removeClass('active');
-			$('div.filter-choose-all').addClass('active');
-			$('.profile').show();
+	function filtersSame(filtersObject){
+		last_value = Object.values(filtersObject)[1];
+		for(var i = 2; i < Object.values(filtersObject).length; i++){
+				var this_value = Object.values(filtersObject)[i];
+				if(last_value != this_value) return false;
+				last_value = this_value;
 		};
+		return true;
+	};
+
+	var app = angular.module("myApp", []);
+	app.controller('myCtrl', function($scope, $timeout){
+		$scope.filters = {
+			'all': true,
+			'webDeveloper': false,
+			'graphicDesigner': false,
+			'marketing': false,
+			'video': false,
+			'iosDeveloper': false,
+			'socialMedia': false,
+			'androidDeveloper': false
+		};
+
+		$('.filter-choose').change(function(){
+			var $this = $(this);
+			$timeout(function(){
+				var this_filter = $this.attr('id');
+				if(this_filter == 'all'){
+					if($scope.filters.all){
+						Object.keys($scope.filters).slice(1).forEach(function(element){
+							$scope.filters[element] = false;
+						});
+					}
+					else{
+						$scope.filters.all = true;
+					}
+				}
+				else{
+					$scope.filters.all = false;
+					if(filtersSame($scope.filters)){
+						$scope.filters.all = true;
+						Object.keys($scope.filters).slice(1).forEach(function(element){
+							$scope.filters[element] = false;
+						});
+					}
+				}
+			}, 1);
+		});
 	});
 };
 
